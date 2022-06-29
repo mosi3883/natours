0e631f6733b393e3db01ad30fd42837bdcd202e9
Mostafa Bodagh Abadi <54463682+mosi3883@users.noreply.github.com>
2022-06-29 07:23:55 +0430

    routing post and get 6-7

--- app.js
+++ app.js
@@ -1,15 +1,56 @@
+const fs = require('fs');
+
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
 // Routing
-app.get('/', (req, res) => {
-  // res.send('Hello from the server side'); // simple sending text respond
-  // res.status(200).send('Hello from the server side'); // sending text with status code
-  res.status(200).json({ message: 'hello from the server', app: 'Natours' }); // sending json
+// app.get('/', (req, res) => {
+//   // res.send('Hello from the server side'); // simple sending text respond
+//   // res.status(200).send('Hello from the server side'); // sending text with status code
+//   res.status(200).json({ message: 'hello from the server', app: 'Natours' }); // sending json
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('you can post to this end point');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.status(200).send('you can post to this end point');
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    'utf-8',
+    (err) => {
+      if (err) {
+      }
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 // creating server
