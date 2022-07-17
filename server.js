3c4e6740b7a0f5e52735b48d4844b736d233c21c
Mostafa Bodagh Abadi <mosi3883@gmail.com>
2022-07-17 15:39:23 +0430

    connecting to mongo atlas using mongoose

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 // server.js for ENV and server and database configuration and error handling
+const mongoose = require('mongoose');
 require('dotenv').config();
 const app = require('./app');
+
+const dbString = process.env.DATABASE.replace('<USERNAME>', process.env.DB_USER)
+  .replace('<PASSWORD>', process.env.DB_PASSWORD)
+  .replace('<DBNAME>', process.env.DB_NAME);
+
+const main = async () => {
+  try {
+    const conn = await mongoose.connect(dbString);
+    console.log(conn.connections);
+  } catch (err) {
+    console.log(`connection error (${err.message})`);
+  }
+};
+main();
 // creating server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
