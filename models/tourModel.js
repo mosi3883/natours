6cfc5081042f984cb46c45ae369b48f8dac2f258
Mostafa Bodagh Abadi <54463682+mosi3883@users.noreply.github.com>
2022-08-06 22:33:58 +0430

    modeling the tour(schema) 8-12

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,16 +5,53 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name!'],
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'a tour must have duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'a tour must have group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'a tour must have difficulty'],
   },
   // rating: Number,
-  rating: {
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a image cover'],
+  },
+  images: [String], //array of string
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // creating model
